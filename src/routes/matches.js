6365a25dc6ecf22a0ac84571313ddce06573494b
from rojan <from rojan>
2026-02-15 16:43:54 +0800

    fixed potential issues

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -17,7 +17,7 @@ matchesRouter.get("/", async (req, res) => {
   if (!parsed.success) {
     return res.status(400).json({
       error: "Invalid query.",
-      details: JSON.stringify(parsed.error),
+      details: parsed.error.issues,
     });
   }
 
@@ -40,16 +40,17 @@ matchesRouter.get("/", async (req, res) => {
 
 matchesRouter.post("/", async (req, res) => {
   const parsed = createMatchSchema.safeParse(req.body);
-  const {
-    data: { startTime, endTime, homeScore, awayScore },
-  } = parsed;
 
   if (!parsed.success) {
     return res.status(400).json({
       error: "Invalid payload.",
-      details: JSON.stringify(parsed.error),
+      details: parsed.error.issues,
     });
   }
+
+  const {
+    data: { startTime, endTime, homeScore, awayScore },
+  } = parsed;
 
   try {
     const [event] = await db
